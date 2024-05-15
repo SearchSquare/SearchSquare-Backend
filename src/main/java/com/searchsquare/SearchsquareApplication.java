86becede646ef1b8944a58104f84218196038cd4
simhani1 <simhani1@gmail.com>
2024-05-15 23:59:34 +0900

    init: 프로젝트를 업로드한다

---
+++ src/main/java/com/searchsquare/SearchsquareApplication.java
@@ -0,0 +1,13 @@
+package com.searchsquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchsquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchsquareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/searchsquare/SearchsquareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchsquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchsquareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
