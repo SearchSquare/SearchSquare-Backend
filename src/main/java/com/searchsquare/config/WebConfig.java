312e9f0e41c91ba041898c63cd5925de4c335143
simhani1 <simhani1@gmail.com>
2024-05-20 03:07:02 +0900

    feat: add jwt interceptor

--- src/main/java/com/searchsquare/config/WebConfig.java
+++ src/main/java/com/searchsquare/config/WebConfig.java
@@ -1,14 +1,20 @@
 package com.searchsquare.config;
 
+import com.searchsquare.core.interceptor.JwtInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@RequiredArgsConstructor
 @EnableWebMvc
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtInterceptor jwtInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -19,5 +25,12 @@ public class WebConfig implements WebMvcConfigurer {
                 HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
                 HttpMethod.PATCH.name())
             .maxAge(1800);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor)
+            .addPathPatterns("/house/**")
+            .excludePathPatterns("/members/oauth/**", "/swagger-ui.html");
     }
 }
