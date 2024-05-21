a18638cb03b32a783fdd6e2c1e9e9e12c43ff843
Jonghan Sim <simhani1@gmail.com>
2024-05-21 11:58:50 +0900

    feat: 아파트 거래내역 조회 API를 추가한다 (#14)

---
+++ src/main/java/com/searchsquare/house/service/dto/HouseDealDto.java
@@ -0,0 +1,21 @@
+package com.searchsquare.house.service.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class HouseDealDto {
+
+    private int houseDealId;
+    private String seller;
+    private String area;
+    private String floor;
+    private int price;
+    private String dealDate;
+}
